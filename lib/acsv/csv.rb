7021341b5a87b61d94e30a62f3d2aac13e4087a2
LeSim <mail@simon.lehericey.net>
2021-11-26 15:18:40 +0100

    separate positional and keyword args (ruby 3)
    see https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0

--- lib/acsv/csv.rb
+++ lib/acsv/csv.rb
@@ -15,7 +15,7 @@ module ACSV
     # @see http://www.ruby-doc.org/stdlib/libdoc/csv/rdoc/CSV.html#method-c-new
     def initialize(data, options = Hash.new)
       options[:col_sep] ||= ACSV::Detect.separator(data)
-      super(data, options)
+      super(data, **options)
     end
 
     # This method opens an IO object, and wraps that with CSV. For reading, separator
@@ -39,7 +39,7 @@ module ACSV
       mode, ext_enc, int_enc = full_mode.split(':')
       if (ext_enc.nil? || ext_enc=='') && options[:encoding].nil? && options[:external_encoding].nil?
         # try to detect encoding
-        if ext_enc = ACSV::Detect.encoding(File.open(args[0], mode, options), options)
+        if ext_enc = ACSV::Detect.encoding(File.open(args[0], mode, **options), options)
           # workaround for http://stackoverflow.com/a/20723346
           ext_enc = "BOM|#{ext_enc}" if ext_enc =~ /UTF/
           # create new mode specification if there was one, else store in option
@@ -58,8 +58,7 @@ module ACSV
       options.delete :confidence
       options.delete :method
       # to superclass
-      args << options
-      super(*args)
+      super(args[0], args[1], **options)
     end
   end
 end
